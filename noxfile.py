e230f9f204169c88e9b0da39cf61a680981e12ea
Patrick Austin <patrick.austin@stfc.ac.uk>
2021-08-03 14:56:45 +0100

    Add linting, formatting, safety

---
+++ noxfile.py
@@ -0,0 +1,59 @@
+import nox
+import tempfile
+
+
+nox.options.sessions = "lint", "tests", "safety"
+locations = "src", "tests", "noxfile.py"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session(python="3.6")
+def tests(session):
+    session.run("poetry", "install", "--no-dev", external=True)
+    session.run("pytest", "--cov")
+
+
+@nox.session(python="3.6")
+def lint(session):
+    args = session.posargs or locations
+    install_with_constraints(session,
+                             "flake8",
+                             "flake8-bandit",
+                             "flake8-black",
+                             "flake8-bugbear",
+                             "flake8-import-order")
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.6")
+def black(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+@nox.session(python="3.6")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
